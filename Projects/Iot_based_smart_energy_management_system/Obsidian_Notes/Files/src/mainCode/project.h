4fa1e138ddf681f4853a8a25a6fcc863d7fc98cf
aruncs31s <aruncs31ss@gmail.com>
2024-04-26 12:09:54 +0530

    backup

--- Projects/Iot_based_smart_energy_management_system/Obsidian_Notes/Files/src/mainCode/project.h
+++ Projects/Iot_based_smart_energy_management_system/Obsidian_Notes/Files/src/mainCode/project.h
@@ -1,6 +1,25 @@
+#include <vector>
 class Project {
 public:
   struct pins {
-    int p1, p2, p3, p4;
+    struct LDR {
+
+      int p1, p2, p3, p4;
+      std::vector<int> avg_LDR(int p1, int p2, int p3, int p4);
+    } LDR;
+    struct Servo {
+
+      int p1, p2, p3, p4;
+    } Servo;
+    struct Relay {
+
+      int p1, p2, p3, p4;
+    } Relay;
+
   } pins;
+  std::vector<int> avg_LDR(int p1, int p2, int p3, int p4) {
+    std::vector<int> arr = {p1, p2, p3, p4};
+    return arr;
+  }
+  Project() {}
 };
